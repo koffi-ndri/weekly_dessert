a371e39eaa62cd8306f2e576da285ed338580c45
Andrew N'dri <koffi.ndri@amalitech.org>
2022-01-20 16:29:52 +0000

    Initial commit

---
+++ urls.py
@@ -0,0 +1,19 @@
+from django.urls import path, include
+
+from . import views
+
+urlpatterns = [
+	path("", views.index, name="index"),
+  # Add a path to your auth links below:
+  
+	# Add a path to your signup request below:
+
+	# Add a path to your logout request below:
+
+	path("polls/<int:pk>/", views.DetailsView.as_view(), name="detail"),
+	path("polls/<int:pk>/results/", views.ResultsView.as_view(), name="results"),
+	path("polls/<int:week_id>/vote/", views.vote, name="vote"),
+  path("account/", include("django.contrib.auth.urls")),
+  path("signup/", views.SignUp.as_view(), name="signup"),
+  path("logout/", views.logout, name="logout"),
+]
